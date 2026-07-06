b790b5f7c734f86353bf991ee3df6dbb1879cc17
yanhui zhang <282809395@qq.com>
2026-07-06 18:30:37 +0800

    实现冒泡排序：对数组进行升序排序

--- main.c
+++ main.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 
+void sort(int arr[], int size)
+{
+    int tmp = 0;
+    for(int i = 0; i < size-1; ++i)
+    {
+        for(int j = 0; j < size-1-i; ++j)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
+}
+
 int main()
 {
     int arr[] = {12,4,89,43,221,78};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    printf("数组长度：%d\n", size);
+    sort(arr, size);
+
     for(int i = 0; i < size; i++)
     {
         printf("%d ", arr[i]);
     }
+    printf("\n");
     return 0;
 }
